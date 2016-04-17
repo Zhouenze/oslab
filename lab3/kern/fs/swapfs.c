cd2bed4a577e23a737334811c593112b38d8e87a
s0255 <zhouez_thu_12@126.com>
2016-04-17 21:03:20 +0800

    lab3

---
+++ lab3/kern/fs/swapfs.c
@@ -0,0 +1,35 @@
+#include <swap.h>
+#include <swapfs.h>
+#include <mmu.h>
+#include <fs.h>
+#include <ide.h>
+#include <pmm.h>
+#include <assert.h>
+#include "fs.h"
+#include "../mm/mmu.h"
+#include "../debug/assert.h"
+#include "../driver/ide.h"
+#include "../mm/swap.h"
+#include "../mm/memlayout.h"
+#include "../../libs/defs.h"
+#include "../mm/pmm.h"
+
+void
+swapfs_init(void) {
+    static_assert((PGSIZE % SECTSIZE) == 0);
+    if (!ide_device_valid(SWAP_DEV_NO)) {
+        panic("swap fs isn't available.\n");
+    }
+    max_swap_offset = ide_device_size(SWAP_DEV_NO) / (PGSIZE / SECTSIZE);
+}
+
+int
+swapfs_read(swap_entry_t entry, struct Page *page) {
+    return ide_read_secs(SWAP_DEV_NO, swap_offset(entry) * PAGE_NSECT, page2kva(page), PAGE_NSECT);
+}
+
+int
+swapfs_write(swap_entry_t entry, struct Page *page) {
+    return ide_write_secs(SWAP_DEV_NO, swap_offset(entry) * PAGE_NSECT, page2kva(page), PAGE_NSECT);
+}
+
